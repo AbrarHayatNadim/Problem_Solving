a53b8ceedc7075f75bf0e6b295df145369a35005
AbrarHayatNadim <abrarnadim247@gmail.com>
2022-01-12 18:05:21 +0600

    second commit

--- validate_anagram.py
+++ validate_anagram.py
@@ -1,1 +1,10 @@
-print("hello")
+from collections import Counter
+
+def are_anagrams(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    return Counter(s1) == Counter(s2)
+    return True
+
+
+print(are_anagrams('dngee', 'gaeden'))
